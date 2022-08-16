934954c3324b392269f70e189709a7c2ee092e18
crack-von-wiegenlied <110370914+crack-von-wiegenlied@users.noreply.github.com>
2022-08-16 13:28:04 +0000

    indexページ作成

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,15 @@
 class Book < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  
+  has_one_attached :book_image
+
+  def get_book_image(width, height)
+    unless book_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      book_image.attach(io: File.open(file_path), filename: 'default-image.jpg',content_type: 'image/jpeg')
+    end
+    book_image.variant(resize_to_limit: [width,height]).processed
+  end
 
 end
